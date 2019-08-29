db0cfbd745f21df9f1e16ecc4cd32e97f7342789
nixuebing <nixuebing@tvflnet.com>
2019-08-29 15:34:14 +0800

    目录结构调整

--- overview/src/main/java/com/ice/imooc/OverviewApplication.java
+++ overview/src/main/java/com/ice/imooc/OverviewApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ImoocApplication {
+public class OverviewApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ImoocApplication.class, args);
+        SpringApplication.run(OverviewApplication.class, args);
     }
 
 }
--- overview/src/test/java/com/ice/imooc/OverviewApplicationTests.java
+++ overview/src/test/java/com/ice/imooc/OverviewApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class ImoocApplicationTests {
+public class OverviewApplicationTests {
 
     @Test
     public void contextLoads() {
