4c9a501e8c3f08ae7c275218fc5f2c3b000fe3db
nixuebing <nixuebing@tvflnet.com>
2019-09-04 17:26:25 +0800

    spring Boot MVC thymeleaf

--- springboot-view/src/main/java/com/ice/imooc/web/config/WebMvcConfig.java
+++ springboot-view/src/main/java/com/ice/imooc/web/config/WebMvcConfig.java
@@ -1,28 +1,56 @@
 package com.ice.imooc.web.config;
 
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.websocket.servlet.TomcatWebSocketServletWebServerCustomizer;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
 import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
 
+import java.io.File;
+
 /**
- * TODO...
+ * Web MVC 配置类
  *
  * @author : ACE
  * @date : 2019/9/3
  */
 @Configuration
-public class WebMvcConfig {
+public class WebMvcConfig implements WebMvcConfigurer {
 
     @Bean
-    public ViewResolver viewResolver() {
+    public ViewResolver myViewResolver() {
         InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
         viewResolver.setViewClass(JstlView.class);
         viewResolver.setOrder(Ordered.LOWEST_PRECEDENCE - 10);
+        viewResolver.setPrefix("/WEB-INF/jsp/");
+        viewResolver.setSuffix(".jsp");
+        viewResolver.setContentType("text/xml;charset=UTF-8");
         return viewResolver;
     }
 
+
+    @Bean
+    public WebServerFactoryCustomizer<TomcatServletWebServerFactory> customizer() {
+        return ((factory) ->
+                factory.addContextCustomizers((context) -> {
+                    File docBaseFile = new File("springboot-view/src/main/webapp");
+                    if (docBaseFile.exists()) {
+                        context.setDocBase(docBaseFile.getAbsolutePath());
+                    }
+                })
+        );
+    }
+
+
+    @Override
+    public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+        configurer.favorParameter(true)
+                .favorPathExtension(true);
+    }
 }
