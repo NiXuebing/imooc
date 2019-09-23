63b50f10c63a4c51c1bfd6f37b54ecccc719e77f
nixuebing <nixuebing@tvflnet.com>
2019-09-23 15:17:30 +0800

    mybatis

---
+++ springboot-mybatis/src/main/java/com/ice/imooc/SpringApplicationBootstrap.java
@@ -0,0 +1,18 @@
+package com.ice.imooc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 项目引导类
+ *
+ * @author : ACE
+ * @date : 2019/9/23
+ */
+@SpringBootApplication
+public class SpringApplicationBootstrap {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringApplicationBootstrap.class, args);
+    }
+}
